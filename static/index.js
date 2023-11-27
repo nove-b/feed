7c0380cdb50551b8959a2efc633f15115c37c6d8
nove-b <info@nove-web.com>
2023-11-27 15:46:49 +0900

    commit

--- static/index.js
+++ static/index.js
@@ -2,6 +2,7 @@ const overviewTrigers = document.querySelectorAll('.js-open-overview')
 if (overviewTrigers.length > 0) {
   overviewTrigers.forEach((overviewTriger) => {
     overviewTriger.addEventListener('mouseenter', (event) => {
+      console.log(event)
       console.log(event.nextElementSibling)
       const overview = event.nextElementSibling
       overview.classlist.classlist.add('show')
