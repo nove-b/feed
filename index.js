dfc2b45e13000002a9e740f7465761a8de0403e7
nove-b <nove-b@users.noreply.github.com>
2023-11-27 06:30:12 +0000

    deploy: dff8258d4cc7990f22539dcce6df29d20c343120

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log('hello')
+const overviewTrigers = document.querySelectorAll('.js-open-overview')
+if (overviewTrigers.length > 0) {
+  overviewTrigers.forEach((overviewTriger) => {
+    overviewTriger.addEventListener('hover', (el) => {
+      console.log(el)
+    })
+  })
+}
