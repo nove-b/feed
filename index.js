46d9da60ba17171c98dfc6b314d046de40d25f55
nove-b <nove-b@users.noreply.github.com>
2023-11-27 06:44:47 +0000

    deploy: 0f8c4844e479ad5b777a2ac6a6beeef811848bb3

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const overviewTrigers = document.querySelectorAll('.js-open-overview')
 if (overviewTrigers.length > 0) {
   overviewTrigers.forEach((overviewTriger) => {
     overviewTriger.addEventListener('mouseenter', (event) => {
-      console.log(event)
+      console.log(event.nextElementSibling)
+      const overview = event.nextElementSibling
+      overview.classlist.classlist.add('show')
     })
   })
 }
