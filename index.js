fc4f394e6ef96ae4ba715980e8f8e169553c7856
nove-b <nove-b@users.noreply.github.com>
2023-11-27 07:06:34 +0000

    deploy: 15075126f635d311b518fb09899916658bbe6405

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-const overviewTrigers = document.querySelectorAll('.js-open-overview')
+const overviewTrigers = document.querySelectorAll('.js-open-overview');
+
 if (overviewTrigers.length > 0) {
   overviewTrigers.forEach((overviewTriger) => {
     overviewTriger.addEventListener('mouseenter', (event) => {
-      const overview = event.target.nextElementSibling
-      overview.classList.add('show')
-    })
+      const overview = event.target.nextElementSibling;
+      overview.classList.add('show');
+    });
 
-    overviewTriger.addEventListener('mouseleave ', (event) => {
-      const overview = event.target.nextElementSibling
-      overview.classList.remove('show')
-    })
-  })
-
+    overviewTriger.addEventListener('mouseleave', (event) => {
+      const overview = event.target.nextElementSibling;
+      overview.classList.remove('show');
+    });
+  });
 }
